3d59ea139232866952af5edb9799f42254682b21
0Viox0 <m.d.mihailik@gmail.com>
2025-12-03 20:02:17 +0300

    feat: add some ocmponents

---
+++ src/shared/icons/MethologyDefaultIcon.tsx
@@ -0,0 +1,19 @@
+export const MethologyDefaultIcon = () => {
+    return (
+        <svg
+            width="18"
+            height="20"
+            viewBox="0 0 18 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M17.0001 10L9.00012 15.4L1.00012 10M17.0001 13.6L9.00012 19L1.00012 13.6M17.0001 6.4L9.00012 11.8L1.00012 6.4L9.00012 1L17.0001 6.4Z"
+                stroke="white"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>
+    );
+};
---
+++ src/shared/icons/index.ts
@@ -0,0 +1,7 @@
+export { BrainstormActive } from './BrainstormActive';
+export { BrainstormDisabled } from './BrainstormDisabled';
+export { MethologyDefaultIcon } from './MethologyDefaultIcon';
+export { ScamperLogo } from './ScamperLogo';
+export { SetDefaultOptionIcon } from './SetDefaultOptionIcon';
+export { SixHatsLogo } from './SixHatsLogo';
+export { TextDefaultIcon } from './TextDefaultIcon';
